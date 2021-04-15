c19ea5aea185679a465c07c678ad4323da15bff1
ashikada <shikada2@illinois.edu>
2021-04-14 22:58:59 -0500

    go back to previous commit

---
+++ my-app/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ledOn: false };
+  }
+
+  setLedState(state) {
+    this.setState({ ledOn: state !== '0' })
+  }
+
+  componentDidMount() {
+    fetch('/led')
+      .then(response => response.text())
+      .then(state => this.setLedState(state));
+  }
+
+  handleStateChange(ledOn) {
+    fetch('/led', { method: 'PUT', body: ledOn ? '0' : '1' })
+      .then(response => response.text())
+      .then(state => this.setLedState(state));
+  }
+  
+  render() {
+    return(
+      <React.Fragment>
+        
+
+        <Dropdown>
+          <DropdownToggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </DropdownToggle>
+
+          <DropdownMenu>
+            {/* href="#/action-1" for link to new webpage */}
+            <DropdownItem>Action</DropdownItem>
+            <DropdownItem>Another Action</DropdownItem>
+            <DropdownItem>Something else</DropdownItem>
+          </DropdownMenu>
+          
+        </Dropdown>
+      </React.Fragment>
+    );
+  }
+
+  /*
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ToggleButton
+            value={this.state.ledOn}
+            onToggle={value => this.handleStateChange(value)}
+          />
+        </header>
+      </div>
+    );
+  }
+  */
+}
+
+export default App;
